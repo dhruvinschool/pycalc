9b7dfe513795e059d9c64ffb9afd173f7822adda
dhruvinschool <19.dhr.bam@ahs.avanti.org.uk>
2025-07-14 09:38:56 +0100

    Update main.py
    Addition of button definitions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import tkinter.messagebox # type: ignore
 from tkinter import *
 
-switch = True
+switch = False
 #this will be used in order to switch radians and degrees mode
 win = Tk()
 #this makes a window
@@ -100,6 +100,9 @@ def btnplus():
     #This uses the length as a position to start with , so after the end of the textbox it will add the operator
 
 def btnsub():
+    if textbox.get() == '0':
+        textbox.delete(0,END)
+    #Clears textbox for negative numbers
     position=len(textbox.get())
     #This finds the length of the textbox
     textbox.insert(position,'-')
@@ -258,12 +261,70 @@ def equals():
     textbox.insert(0, str(ans))
     #The answer to the evaluation is inputted into the start of the textbox
 
+def open_parentheses():
+    if textbox.get() == '0':
+        textbox.delete(0,END)
+    #This if statement checks for if the textbox is equal to zero , if it is it wipes it to add a parentheses.
+    position=len(textbox.get())
+    textbox.insert(position,'(')
+    #A parentheses is added into the textbox.
+
+def closing_parentheses():
+    if textbox.get() == '0':
+        textbox.delete(0,END)
+    #This if statement checks for if the textbox is equal to zero , if it is it wipes it to add a parentheses.
+    position=len(textbox.get())
+    textbox.insert(position,')')
+    #A parentheses is added into the textbox.
+
+
+def delete():
+    if textbox.get() == '0':
+        textbox.delete(0,END)
+        textbox.insert(0,'0')
+    #In this if statement , we are checking to see if the textbox is equal to 0. If it is , we do NOT delete the 0 and we keep it as it is , as we do not want an empty textbox.
+    else:
+        position=len(textbox.get())
+        textbox.delete(position-1,position)
+    #In this else statement , since the textbox isnt equal to zero , the last digit on the textbox is deleted to shorted the textbox.
+def decimal():
+    position = len(textbox.get())
+    textbox.insert(position,'.')
+    #Here , a decimal is simply added to the end of the textbox.
+
+def sqr():
+    position=len(textbox.get())
+    textbox.insert(position,'**2')
+    #Adds **2 to the end of the textbox. The evaluate function interprets this as ^2.
+
+def power():
+    position=len(textbox.get())
+    textbox.insert(position, '**')
+    ##Adds ** to the end of the textbox. The evaluate function interprets this as ^ , so the next number is the exponent.
+
+def fact():
+    try:
+        ans=textbox.get()
+        ans = math.factorial(int(ans))
+        textbox.delete(0,END)
+        textbox.insert(0,str(ans))
+    #This essentially finds the factorial of the textbox , with the entire textbox being interpreted as a single integer. This is then outputted into the box.
+    except Exception:
+        tkinter.messagebox.showerror("Error , check operand and operation")
+    #If the textbox is not in integer form , or is a negative number , this error is reported.
+
+
+
+
 textbox = Entry(win,font='Arial 20',fg="Black",bg="#cdd6f4",bd=4,justify=RIGHT)
 #This is where the textbox itself is created , I used Arial font and black foreground for black text. The background colour has been chosen from a colorscheme online (Catppuccin mocha)
 textbox.insert(0, '0')
 #This makes the default textbox value 0
 textbox.pack(expand=TRUE, fill=BOTH)
 #This makes a textbox , which will afterwards be the answer box.
+
+#All the following blocks of code are in the same format. The first line makes a row , which is just a frame in the window. The row is made expandable , and buttons are all added to the row to make actual buttons for the calculator.
+
 row1=Frame(win,bg="#1e1e2e")
 row1.pack(expand=TRUE, fill=BOTH)
 onebtn=Button(row1,text="1",font="Arial 23",relief="groove", command=btn1, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
@@ -273,8 +334,8 @@ plusbtn=Button(row1,text="+",font="Arial 23",relief="groove", command=btnplus,fg
 sinbtn=Button(row1,text="sin",font="Arial 18",relief="groove", command=sin,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 cosbtn=Button(row1,text="cos",font="Arial 18",relief="groove", command=cos,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 tanbtn=Button(row1,text="tan",font="Arial 18",relief="groove", command=tan,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-openbtn=Button(row1,text="(",font="Arial 18",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-closebtn=Button(row1,text=")",font="Arial 18",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+openbtn=Button(row1,text="(",font="Arial 18",relief="groove", command=open_parentheses, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+closebtn=Button(row1,text=")",font="Arial 18",relief="groove", command=closing_parentheses, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 
 row2=Frame(win,bg="#1e1e2e")
 row2.pack(expand=TRUE, fill=BOTH)
@@ -286,7 +347,7 @@ arcsinbtn=Button(row2,text="sin-1",font="Arial 10 bold",relief="groove", command
 arccosbtn=Button(row2,text="cos-1",font="Arial 10 bold",relief="groove", command=acos,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 arctanbtn=Button(row2,text="tan-1",font="Arial 10 bold",relief="groove", command=atan,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 pibtn=Button(row2,text="π",font="Arial 18",relief="groove", command=pi,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-factbtn=Button(row2,text="x!",font="Arial 18",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+factbtn=Button(row2,text="x!",font="Arial 18",relief="groove", command=fact, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 
 row3=Frame(win,bg="#1e1e2e")
 row3.pack(expand=TRUE, fill=BOTH)
@@ -294,22 +355,23 @@ sevenbtn=Button(row3,text="7",font="Arial 23", command=btn7,relief="groove",fg="
 eightbtn=Button(row3,text="8",font="Arial 23", command=btn8,relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 ninebtn=Button(row3,text="9",font="Arial 23", command=btn9,relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 multiplybtn=Button(row3,text="*",font="Arial 23",relief="groove", command=btnmult,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-sqrbtn=Button(row3,text="x^2",font="Arial 17",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+sqrbtn=Button(row3,text="x^2",font="Arial 17",relief="groove", command=sqr, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 logbtn=Button(row3,text="log",font="Arial 17",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 lnbtn=Button(row3,text="ln",font="Arial 17",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 sqrrootbtn=Button(row3,text="√x",font="Arial 17",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-sqrbtn=Button(row3,text="x^y",font="Arial 17",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+pwrbtn=Button(row3,text="x^y",font="Arial 17",relief="groove", command=power, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 
 row4=Frame(win,bg="#1e1e2e")
 row4.pack(expand=TRUE, fill=BOTH)
-backbtn=Button(row4,text="⌫",font="Arial 15",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
+backbtn=Button(row4,text="⌫",font="Arial 15",relief="groove", command=delete, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
 zerobtn=Button(row4,text="0",font="Arial 23",relief="groove", command=btn0,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 equalsbtn=Button(row4,text="=",font="Arial 11 bold",relief="groove", command=equals, fg="white",bg='#89b4fa').pack(side=LEFT, expand=TRUE, fill=BOTH)
 dividebtn=Button(row4,text="/",font="Arial 23",relief="groove", command=btndiv,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 clearbtn=Button(row4,text="AC",font="Arial 20",relief="groove", command=ac,fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
-decimalbtn=Button(row4,text=".",font="Arial 20",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
-placeholder1btn=Button(row4,text="n/a",font="Arial 20",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
-switcheroooo=Button(row4,text="n/a",font="Arial 20", relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+decimalbtn=Button(row4,text=".",font="Arial 20",relief="groove", command=decimal, fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)    
+ebtn=Button(row4,text="e",font="Arial 20",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+placeholder2btn=Button(row4,text="n/a",font="Arial 20",relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
+switcheroooo=Button(row4,text="d/r",font="Arial 20", relief="groove",fg="white",bg='#1e1e2e').pack(side=LEFT, expand=TRUE, fill=BOTH)
 
 win.mainloop()
 #This is what actually starts the window
